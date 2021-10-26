c9aecb3a3eeccfca82abad42316dda0ba9ffa4c1
Satyam Kumar <satyam6623165@gmail.com>
2021-10-26 11:52:37 +0530

    implement lamport counter ⏱ (#8)
    * implement lamport counter ⏱
    
    * refactor lamport clock
    
    * add license headers and run goimports and go fmt
    
    * Update clock_test.go
    
    remove unnecessary newline
    
    * Update operations/lamport/clock_test.go
    
    Co-authored-by: Bisakh <bisakhmondal00@gmail.com>
    
    Co-authored-by: Bisakh <bisakhmondal00@gmail.com>

---
+++ operations/lamport/clock.go
@@ -0,0 +1,44 @@
+//  Copyright 2021, athena-crdt authors.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package lamport
+
+import (
+	"sync/atomic"
+)
+
+type Clock struct {
+	counter uint64
+}
+
+func (clock *Clock) Increment() uint64 {
+	return atomic.AddUint64(&clock.counter, 1)
+}
+
+func (clock *Clock) CompareAndUpdate(value uint64) {
+	for {
+		cur := atomic.LoadUint64(&clock.counter)
+
+		if value < cur {
+			return
+		}
+		if atomic.CompareAndSwapUint64(&clock.counter, cur, value+1) {
+			break
+		}
+	}
+}
+
+func (clock *Clock) GetTime() uint64 {
+	return atomic.LoadUint64(&clock.counter)
+}
