5b07464e88840a0981b621e4f97e75427c58e5a0
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-10-27 12:31:01 +0530

    json node schema definition

---
+++ operations/defs/list_node.go
@@ -0,0 +1,59 @@
+//  Copyright 2021, athena-crdt authors.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package defs
+
+import "github.com/athena-crdt/athena-core/utils"
+
+type ListNode struct {
+	*baseNode
+	listIndex uint64
+}
+
+// NewListNode returns a Node of type ListNode.
+func NewListNode(id ID) *ListNode {
+	return &ListNode{
+		baseNode: newBaseNode(id),
+	}
+}
+
+func (l *ListNode) Clone() (Node, error) {
+	return utils.DeepCopy(l)
+}
+
+// Index returns the current index of ListNode.
+func (l *ListNode) Index() uint64 {
+	return l.listIndex
+}
+
+// SetIndex sets the index of currentNode with the given index.
+func (l *ListNode) SetIndex(idx uint64) {
+	l.listIndex = idx
+}
+
+func (l *ListNode) InsertAtHead(child Node) error {
+	panic("implement")
+}
+
+func (l *ListNode) InsertAfter(id string, child Node) error {
+	panic("implement")
+}
+
+func (l *ListNode) Serialize() error {
+	panic("implement me")
+}
+
+func (l *ListNode) Deserialize(bytes []byte) error {
+	panic("implement me")
+}
---
+++ operations/defs/register_node.go
@@ -0,0 +1,66 @@
+//  Copyright 2021, athena-crdt authors.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package defs
+
+import "github.com/pkg/errors"
+
+type RegisterNode struct {
+	*baseNode
+	value interface{}
+}
+
+func (r *RegisterNode) Clone() (Node, error) {
+	return NewRegisterNode(r.id, r.value), nil
+}
+
+// SetValue assigns the current node with the given value.
+func (r *RegisterNode) SetValue(val interface{}) {
+	r.value = val
+}
+
+// Value returns the current RegisterNode value.
+func (r *RegisterNode) Value() interface{} {
+	return r.value
+}
+
+// FetchChild override. RegisterNode is always a leaf node.
+func (r *RegisterNode) FetchChild([]ID) (Node, error) {
+	return nil, errors.New("RegisterNode doesn't have a children set")
+}
+
+// Child returns a nil object, as RegisterNode itself is a leaf node.
+func (r *RegisterNode) Child() Children {
+	return nil
+}
+
+// NewRegisterNode returns a Node of type RegisterNode with given id and value.
+func NewRegisterNode(id ID, value interface{}) *RegisterNode {
+	return &RegisterNode{
+		baseNode: &baseNode{
+			id:        id,
+			tombstone: false,
+			children:  nil,
+		},
+		value: value,
+	}
+}
+
+func (r *RegisterNode) Serialize() error {
+	panic("implement me")
+}
+
+func (r *RegisterNode) Deserialize(bytes []byte) error {
+	panic("implement me")
+}
